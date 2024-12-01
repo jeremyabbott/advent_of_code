a13df78f4eb7b6f48c0fef2b15af2efa41ca7a08
Jeremy Abbott <jeremy.abbott@pm.me>
2024-12-01 08:57:36 -0800

    Initial Commit

---
+++ src/AdventOfCode/AdventOfCode.fs
@@ -0,0 +1,36 @@
+﻿namespace AdventOfCode
+open System
+
+module Helpers =
+    
+    let readLines path =
+        System.IO.File.ReadAllLines(path)
+
+module Day1 =
+    let getLocations () =
+        let locations =
+            Helpers.readLines "/Users/jeremyabbott/code/advent_of_code/src/AdventOfCode/2024/Day01_Part1.txt"
+            |> Array.map (fun l ->
+                let locationIds = l.Split(' ') |> Array.filter (fun s -> String.IsNullOrEmpty(s) |> not)
+                let first = locationIds.[0].Trim() |> int
+                let second = locationIds.[1].Trim() |> int
+                (first, second))
+            |> Array.unzip
+            |> (fun (a, b) -> (a |> Array.sort, b |> Array.sort))
+        locations
+        
+    let getTotalDistance list1 list2 =
+        Array.zip list1 list2
+        |> Array.map (fun (a:int, b:int) -> Math.Abs(a - b))
+        |> Array.sum
+        
+    let getSimilarityScore (list1: int array) (list2: int array) =
+        list1
+        |> Array.map(fun l ->
+            list2
+            |> Array.where (fun l2 -> l2 = l)
+            |> Array.length
+            |> (fun c -> l * c))
+        |> Array.sum
+        
+        
---
+++ tests/AdventOfCode.Tests/Tests.fs
@@ -0,0 +1,16 @@
+﻿module Tests
+
+open System
+open Xunit
+
+[<Fact>]
+let ``Day1`` () =
+    let (list1, list2) = AdventOfCode.Day1.getLocations()
+    Assert.Equal(1000, list1.Length)
+    Assert.Equal(1000, list2.Length)
+    // Part 1
+    let totalDistance = AdventOfCode.Day1.getTotalDistance list1 list2
+    Assert.Equal(1530215, totalDistance)
+    // Part 2
+    let similarityScore = AdventOfCode.Day1.getSimilarityScore list1 list2
+    Assert.Equal(26800609, similarityScore)
