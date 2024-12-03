e19166131b8af43127bfc5cb4b053c79e0961e25
Jeremy Abbott <jeremy.abbott@pm.me>
2024-12-02 19:47:50 -0800

    Day2

--- src/AdventOfCode/AdventOfCode.fs
+++ src/AdventOfCode/AdventOfCode.fs
@@ -7,9 +7,9 @@ module Helpers =
         System.IO.File.ReadAllLines(path)
 
 module Day1 =
-    let getLocations () =
+    let getLocations input =
         let locations =
-            Helpers.readLines "/Users/jeremyabbott/code/advent_of_code/src/AdventOfCode/2024/Day01_Part1.txt"
+            Helpers.readLines input
             |> Array.map (fun l ->
                 let locationIds = l.Split(' ') |> Array.filter (fun s -> String.IsNullOrEmpty(s) |> not)
                 let first = locationIds.[0].Trim() |> int
@@ -33,4 +33,39 @@ module Day1 =
             |> (fun c -> l * c))
         |> Array.sum
         
+module Day2 =
+    let isInRange i = i >= 1 && i <= 3 || i <= -1 && i >= -3
+    let isNegative i = i < 0
+    let isPositive i = i > 0
+    let isSafe (report: int list) =
+        let deltas =
+            report
+            |> List.pairwise
+            |> List.map(fun (a,b) -> a-b)
+        let allInRange = deltas |> List.forall isInRange
+        let allIncreasing = deltas |> List.forall isPositive
+        let allDecreasing = deltas |> List.forall isNegative
+        allInRange && (allIncreasing || allDecreasing)
         
+    let analyzeReports input =
+        let reports =
+            Helpers.readLines input
+            |> Array.map(fun l -> l.Split(' ') |> Array.map int |> List.ofArray)
+        
+        reports |> Array.filter isSafe |> Array.length
+        
+    let isSafeV2 (report: int list) =
+        let safe = isSafe report
+        let safeWithBuffer =
+            report
+            |> List.mapi (fun i _ ->
+                let remaining = List.removeAt i report
+                isSafe remaining)
+            |> List.exists id
+        safeWithBuffer || safe
+        
+    let analyzeReportsV2 input =
+        let reports =
+            Helpers.readLines input
+            |> Array.map(fun l -> l.Split(' ') |> Array.map int |> List.ofArray)
+        reports |> Array.filter isSafeV2 |> Array.length
--- tests/AdventOfCode.Tests/Tests.fs
+++ tests/AdventOfCode.Tests/Tests.fs
@@ -5,7 +5,7 @@ open Xunit
 
 [<Fact>]
 let ``Day1`` () =
-    let (list1, list2) = AdventOfCode.Day1.getLocations()
+    let (list1, list2) = AdventOfCode.Day1.getLocations "2024/Day01_Part1.txt"
     Assert.Equal(1000, list1.Length)
     Assert.Equal(1000, list2.Length)
     // Part 1
@@ -14,3 +14,23 @@ let ``Day1`` () =
     // Part 2
     let similarityScore = AdventOfCode.Day1.getSimilarityScore list1 list2
     Assert.Equal(26800609, similarityScore)
+    
+[<Fact>]
+let ``Day2 Part 1`` () =
+    let safeReports = AdventOfCode.Day2.analyzeReports "2024/Day02_Part1.txt"
+    Assert.Equal(670, safeReports)
+    
+[<Fact>]
+let ``Day2 Part 2`` () =
+    let safeReports = AdventOfCode.Day2.analyzeReportsV2 "2024/Day02_Part1.txt"
+    Assert.Equal(700, safeReports)
+
+[<Fact>]
+let shouldBeSafe () =
+    let safe = AdventOfCode.Day2.isSafeV2 [8;6;4;4;1]
+    Assert.True(safe)
+    
+[<Fact>]
+let shouldNotBeSafe () =
+    let safe = AdventOfCode.Day2.isSafeV2 [9;7;6;2;1]
+    Assert.False(safe)
